26ccabd5c762204505e59895562e2cc02219d40a
Nikolay <dukarev1@gmail.com>
2022-09-05 20:51:36 +0300

    Initial commit

---
+++ src/main/java/com/example/gbarchitecturehw7/GbArchitectureHw7Application.java
@@ -0,0 +1,13 @@
+package com.example.gbarchitecturehw7;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GbArchitectureHw7Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GbArchitectureHw7Application.class, args);
+    }
+
+}
